48e1391ccf80ed842c09a39a4ae8b8048db0ff04
CODeRUS <coderusinbox@gmail.com>
2016-10-21 00:20:49 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,6 +20,8 @@ fi
 
 intexpkgs="feature-intex sailfish-content-configuration-intex sailfish-content-ambiences-intex sailfish-content-partnerspaces-intex sailfish-content-browser-intex sailfish-content-profiled-settings-intex all-translations-intex-pack sms-activation-intex sailfish-content-graphics-intex sailfish-content-partnerspaces-intex-tutorial sms-activation-intex-conf sailfish-content-ambiences-intex-default-ambience sailfish-content-tones-intex sailfish-content-gallery-configuration-intex sailfish-content-partnerspaces-intex-gaana sailfish-content-graphics-intex-z1.25 splash-img-l500d-intex"
 for pkg in $intexpkgs; do pkcon remove $pkg; done
+sailfishpkgs="sailfish-content-graphics-default-z1.25 sailfish-content-graphics-default"
+for pkg in $sailfishpkgs; do pkcon remove $pkg; done
 pkcon install feature-jolla sailfish-content-configuration-jolla sailfish-content-graphics-jolla-z1.25 jolla-settings-accounts-extensions-3rd-party-all sailfish-content-apps-default-configs splash-img-l500d-jolla
 
 touch /usr/share/ssu/board-mappings.d/10-l500d-jolla.ini
